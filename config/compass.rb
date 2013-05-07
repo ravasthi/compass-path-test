f6119b107a5a727f897446c82e3dc43e0ff07ddc
Richa Avasthi <richa@outright.com>
2013-05-07 09:39:58 -0700

    Initial commit.

---
+++ config/compass.rb
@@ -0,0 +1,10 @@
+require 'sassy-buttons'
+
+additional_import_paths = [
+  'app/assets/stylesheets/basics',
+  'app/assets/stylesheets/shared',
+  'vendor/assets/stylesheets/sass-icon-fonts'
+]
+
+line_comments = false
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassTest::Application.config.secret_token = 'a216eb4681907b5797142c3a12dfe1c1d69dd1295d602b298d53f9f881d3272a23ce88d33f3fdccfee791a614de63ffd5b61e7a8235224f7b5fb3d2f93d40c46'
